39e993d717a35ca46771a0f6d98523f3a3ecee80
Ricardo Rausell <momet6@gmail.com>
2025-08-13 11:11:05 +0200

    Initial Commit

---
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -0,0 +1,60 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+import '../../../config/helpers/human_formats.dart';
+
+class VideoButtons extends StatelessWidget {
+
+  final VideoPost video;
+  const VideoButtons({
+    super.key,
+    required this.video
+
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomIConButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
+        const SizedBox( height: 20),
+        _CustomIConButton(value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox( height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration( seconds: 5),
+          child: const _CustomIConButton(value: 0, iconData: Icons.play_circle_outline)
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomIConButton extends StatelessWidget {
+
+  final int value;
+  final IconData iconData;
+  final Color? color;
+
+  const _CustomIConButton({super.key,
+    required this.value,
+    required this.iconData,
+    iconColor
+  }): color = iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(onPressed: () {},
+            icon: Icon(iconData, color: color, size: 30,)),
+
+        if (value > 0)
+        Text(HumanFormats.humanReadbleNumber(value.toDouble())),
+      ],
+    );
+  }
+}
+
